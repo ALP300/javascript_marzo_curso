2a53ba1e921301eea15efb8470d5d62118a96c46
ALP300 <aitorpalomino1@gmail>
2025-03-06 17:42:45 -0500

    Actualizar

---
+++ EJERCICIO_SEMANA1/VARIABLES/ejer2.js
@@ -0,0 +1,16 @@
+let num1 = parseInt(prompt("Por favor ingresa el número 1: "));
+let num2 = parseInt(prompt("Por favor ingresa el número 2: "));
+let text= "Hola bienvenidos, este es mi primer proyecto en javascript"
+let sum= num1+num2;
+let altura= 15.6;
+let peso= 17.2;
+let ver= true;
+let fal= false;
+let modulo= num1%num2;
+let sum2= altura+peso
+console.log(modulo)
+console.log(sum2)
+console.log(text)
+console.log(num1);
+console.log(num2);
+console.log("La suma es: "+sum);
